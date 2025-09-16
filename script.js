50a88adaa3f70377950931aecdce8837059930e4
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-16 21:02:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const table = document.getElementById("sampleTable");
 
-let cellCount = 0;
 function insert_Row() {
 	const tr = document.createElement("tr");
 
 	const td1 = document.createElement("td");
-	cellCount++;
-	td1.innerText = `New Cell${cellCount}`;
+	td1.innerText = `New Cell1`;
 
 	const td2 = document.createElement("td");
-	cellCount++;
-	td2.innerText = `New Cell${cellCount}`;
+	td2.innerText = `New Cell2`;
 
 	tr.append(td1, td2);
 
